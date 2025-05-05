31ecbe5192fb77005629e774839b6fe52c343864
jaehyun <pjh04100@naver.com>
2025-05-06 02:09:20 +0900

    add - request response gpt data
    emotionselecetactivity에 있는 nextPageButton이 누르면 content ,emotion정보를 gptservice에 있는 gpt api key를 통해 접속하고  gptrequest에 있는 role을 통해 정보가 들어왔다는 것을 확인. 그 후 다시 gptresponse를 통해 응답 받는 식입니다.
    emotionselecetactivity에서 보내는 메시지 형태를 정할 수 있습니다

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    // gpt api 사용
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }

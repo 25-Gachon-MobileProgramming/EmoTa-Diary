13856553f6ba58a736a828e868a9a94fb2778987
아진 <106726806+worhs02@users.noreply.github.com>
2025-05-22 17:31:50 +0900

    Revert "Feature/#83 notification"

--- app/src/main/java/kr/co/gachon/emotion_diary/data/AppDatabase.java
+++ app/src/main/java/kr/co/gachon/emotion_diary/data/AppDatabase.java
@@ -29,19 +29,4 @@ public abstract class AppDatabase extends RoomDatabase {
 
         return INSTANCE;
     }
-    public static AppDatabase getInstance(Context context) {
-        if (INSTANCE == null) {
-            synchronized (AppDatabase.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(
-                            context.getApplicationContext(),
-                            AppDatabase.class,
-                            "emotion-diary-db"
-                    ).build();
-                }
-            }
-        }
-        return INSTANCE;
-    }
-
 }

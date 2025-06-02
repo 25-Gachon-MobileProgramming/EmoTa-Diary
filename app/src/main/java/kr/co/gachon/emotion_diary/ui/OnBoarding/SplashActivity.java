431db849ef8842d1950447e9e777082620f98eae
ez0o43 <zxpsej43@gmail.com>
2025-06-03 01:26:09 +0900

    delete unused code

--- app/src/main/java/kr/co/gachon/emotion_diary/ui/OnBoarding/SplashActivity.java
+++ app/src/main/java/kr/co/gachon/emotion_diary/ui/OnBoarding/SplashActivity.java
@@ -6,20 +6,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.load.resource.gif.GifDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -65,7 +54,7 @@ public class SplashActivity extends AppCompatActivity {
         boolean isAvatarCompleted = prefs.getBoolean("isAvatarCompleted", false);
 
         Intent intent = isAvatarCompleted ?
-                new Intent(this, OnBoardingActivity.class) :
+                new Intent(this, MainActivity.class) :
                 new Intent(this, OnBoardingActivity.class);
 
         startActivity(intent);
